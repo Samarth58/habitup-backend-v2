8683c75f794ae50e3a1ccb62d2a31fd2a1df1511
Samarth HG <samarthahg2004@gmail.com>
2026-08-29 22:52:44 +0530

    refactor: update server entry point configuration and connection handling in server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 
